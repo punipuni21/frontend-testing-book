1246cf71d6e96ac285e25c3087ce41b600ae0c53
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-18 23:25:35 +0900

    conditional branch

--- src/03/04/index.test.ts
+++ src/03/04/index.test.ts
@@ -1,14 +1,16 @@
 import { add, sub } from "./";
 
-test("return value is sum of two arguments", () => {
-  expect(add(50, 50)).toBe(100);
-});
-test("upper limit is 100", () => {
-  expect(add(70, 80)).toBe(100);
-});
-test("sum is subtraction of two arguments", () => {
-  expect(sub(51, 50)).toBe(1);
-});
-test("lower limit is 0", () => {
-  expect(sub(50, 60)).toBe(0);
+describe("conditional branch", () => {
+  test("return value is sum of two arguments", () => {
+    expect(add(50, 50)).toBe(100);
+  });
+  test("upper limit is 100", () => {
+    expect(add(70, 80)).toBe(100);
+  });
+  test("sum is subtraction of two arguments", () => {
+    expect(sub(51, 50)).toBe(1);
+  });
+  test("lower limit is 0", () => {
+    expect(sub(50, 60)).toBe(0);
+  });
 });
