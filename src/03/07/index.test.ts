f452534ded54f7b3d9b1fa1df0985c2836a23433
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-20 23:06:32 +0900

    promise comparision(reject)

--- src/03/07/index.test.ts
+++ src/03/07/index.test.ts
@@ -1,6 +1,6 @@
-import { wait } from ".";
+import { timeout, wait } from ".";
 
-describe("wait verification", () => {
+describe("wait verification(resolve", () => {
   test("when wait for designated time, promise is resolved", () => {
     return wait(50).then((duration) => {
       expect(duration).toBe(50);
@@ -16,6 +16,31 @@ describe("wait verification", () => {
   });
 
   test("when wait for designated time, promise is resolved", async () => {
-    await expect(wait(50)).resolves.toBe(50);
+    expect(await wait(50)).toBe(50);
+  });
+});
+
+describe("wait verification(reject)", () => {
+  test("when wait for designated time, promise is rejected", () => {
+    return timeout(50).catch((duration) => {
+      expect(duration).toBe(50);
+    });
+  });
+
+  test("when wait for designated time, promise is rejected", () => {
+    return expect(timeout(50)).rejects.toBe(50);
+  });
+
+  test("when wait for designated time, promise is rejected", async () => {
+    await expect(timeout(50)).rejects.toBe(50);
+  });
+
+  test("when wait for designated time, promise is rejected", async () => {
+    expect.assertions(1); // 1回のアサーションを行うことを期待
+    try {
+      await timeout(50);
+    } catch (err) {
+      expect(err).toBe(50);
+    }
   });
 });
