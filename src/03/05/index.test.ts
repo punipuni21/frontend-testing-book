bd3aa9e842eabdc1fad03f3c0a048a4cd6cb160a
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-20 22:00:01 +0900

    custom error

--- src/03/05/index.test.ts
+++ src/03/05/index.test.ts
@@ -1,4 +1,17 @@
-import { add } from "./";
-test("when argments are not between 0 and 100, it throws an error", () => {
-  expect(() => add(110, -10)).toThrow("input value must be between 0 and 100");
+import { add, RangeError, sub } from "./";
+
+describe("四則演算", () => {
+  describe("add", () => {
+    test("when argments are not between 0 and 100, it throws an error", () => {
+      expect(() => add(110, -10)).toThrow(
+        "input value must be between 0 and 100"
+      );
+    });
+  });
+
+  describe("sub", () => {
+    test("when argments are not between 0 and 100, it throws an error", () => {
+      expect(() => add(110, -10)).toThrow(RangeError);
+    });
+  });
 });
--- src/03/05/index.ts
+++ src/03/05/index.ts
@@ -1,14 +1,27 @@
-export function add(a: number, b: number): number {
-  if (a < 0 || a > 100) {
-    throw new Error("input value must be between 0 and 100");
-  }
-  if (b < 0 || b > 100) {
-    throw new Error("input value must be between 0 and 100");
-  }
+export class RangeError extends Error {}
 
+function checkRange(value: number) {
+  if (value < 0 || value > 100) {
+    throw new RangeError("input value must be between 0 and 100");
+  }
+}
+
+export function add(a: number, b: number): number {
+  checkRange(a);
+  checkRange(b);
   const sum = a + b;
   if (sum > 100) {
     return 100;
+  }
+  return sum;
+}
+
+export function sub(a: number, b: number): number {
+  checkRange(a);
+  checkRange(b);
+  const sum = a - b;
+  if (sum < 0) {
+    return 0;
   }
   return sum;
 }
