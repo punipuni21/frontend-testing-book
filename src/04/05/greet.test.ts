fb5edc0c8870889eb89041f7ce8d47309e5e0ece
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-21 23:03:57 +0900

    call time verification

--- src/04/05/greet.test.ts
+++ src/04/05/greet.test.ts
@@ -8,3 +8,11 @@ test("mock func was not called", () => {
   const mockFn = jest.fn();
   expect(mockFn).not.toHaveBeenCalled();
 });
+
+test("verify the number of calls", () => {
+  const mockFn = jest.fn();
+  mockFn();
+  expect(mockFn).toHaveBeenCalledTimes(1);
+  mockFn();
+  expect(mockFn).toHaveBeenCalledTimes(2);
+});
