28a180380ef683ca3b0e6a5429013910763849d4
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-22 14:09:18 +0900

    add comment

--- src/05/03/Form.test.tsx
+++ src/05/03/Form.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import { Form } from "./Form";
 
+// @babel/preset-typescript: TypeScriptからJavaScriptへ変換
+// @babel/preset-react: BabelにJSX→JSへ変換してもらう
+// @babel/preset-env : ESModules→CommonJSの変換
+
 test("display name", () => {
   render(<Form name="taro" />);
   expect(screen.getByText("taro")).toBeInTheDocument();
