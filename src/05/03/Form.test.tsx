d8e4532aad59c68dc432eef6a72cc3483212ea71
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-22 11:48:04 +0900

    create test, but failed

--- src/05/03/Form.test.tsx
+++ src/05/03/Form.test.tsx
@@ -2,6 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { Form } from "./Form";
 
 test("display name", () => {
-  render(<Form name="John Doe" />);
-  console.log(screen.getByText("John Doe"));
+  render(<Form name="taro" />);
+  screen.debug();
+  expect(screen.getByText("taro")).toBeInTheDocument();
+});
+
+test("display button", () => {
+  render(<Form name="taro" />);
+  expect(screen.getByRole("button")).toBeInTheDocument();
 });
