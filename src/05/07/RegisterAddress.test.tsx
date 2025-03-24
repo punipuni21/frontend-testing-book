91a38ecad546e216b31405f8b5d915531c9d2443
punipuni21 <watanaberyotaroyp@gmail.com>
2025-03-24 22:26:41 +0900

    success test

--- src/05/07/RegisterAddress.test.tsx
+++ src/05/07/RegisterAddress.test.tsx
@@ -1,13 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { mockPostMyAddress } from "./fetchers/mock";
 import {
   clickSubmit,
   inputContactNumber,
   inputDeliveryAddress,
 } from "./testingUtil";
+import { RegisterAddress } from "./RegisterAddress";
+
+jest.mock("./fetchers");
 
 async function fillValuesAndSubmit() {
   const contactNumber = await inputContactNumber();
-  const deliverAddress = await inputDeliveryAddress();
-  const submitValues = { ...contactNumber, ...deliverAddress };
+  const deliveryAddress = await inputDeliveryAddress();
+  const submitValues = { ...contactNumber, ...deliveryAddress };
   await clickSubmit();
   return submitValues;
 }
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test("成功時「登録しました」が表示される", async () => {
+  const mockFn = mockPostMyAddress();
+  render(<RegisterAddress />);
+  const submitValues = await fillValuesAndSubmit();
+  expect(mockFn).toHaveBeenCalledWith(expect.objectContaining(submitValues));
+  expect(screen.getByText("登録しました")).toBeInTheDocument();
+});
